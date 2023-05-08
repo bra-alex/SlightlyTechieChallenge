5cf029fedc730c204933a5e9c5be0de7fc6d8c17
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-05-08 10:43:54 +0000

    add redirect to docs and healtcare route

--- src/docs/post.route.doc.ts
+++ src/docs/post.route.doc.ts
@@ -119,3 +119,15 @@
  *       400:
  *         description: Bad Request
  */
+
+/**
+ * @openapi
+ * /healthcheck:
+ *  get:
+ *     tags:
+ *     - Healthcheck
+ *     description: Responds if the app is up and running
+ *     responses:
+ *       200:
+ *         description: App is up and running
+ */
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 import swaggerDocs from './utils/swagger'
 import postRouter from './routes/posts.routes'
@@ -12,6 +12,12 @@ const app = express()
 app.use(express.json())
 
 app.use(postRouter)
+
+app.use('/', (_req: Request, res: Response) => {
+  res.redirect('docs')
+})
+
+app.get('/healthcare', (_req: Request, res: Response) => res.sendStatus(200))
 
 app.listen(3000, async () => {
   await mongoConnect()
